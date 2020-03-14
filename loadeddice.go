3b74059c20032f1033f94d51aaac121c44b4a9fb
Scott Rodgers <scott.r.hulme@live.co.uk>
2020-03-14 23:17:22 +0000

    Explicitly exporting WeightedItem properties

--- loadeddice.go
+++ loadeddice.go
@@ -44,7 +44,7 @@ func GetItem(items []WeightedItem) interface{} {
 func sumWeights(items []WeightedItem) int {
 	var total int = 0
 	for i := 0; i < len(items); i++ {
-		total += items[i].weight
+		total += items[i].Weight
 	}
 
 	return total
@@ -54,9 +54,9 @@ func generateWeightRanges(items []WeightedItem, total int) []float64 {
 	var weightRanges []float64
 	var count int = 0
 	for _, item := range items {
-		count += item.weight
+		count += item.Weight
 		var weightRange float64 = -1.0
-		if item.weight > 0 {
+		if item.Weight > 0 {
 			weightRange = (100.0 / float64(total) * float64(count)) / 100
 		}
 		weightRanges = append(weightRanges, weightRange)
@@ -67,6 +67,6 @@ func generateWeightRanges(items []WeightedItem, total int) []float64 {
 
 // WeightedItem consists of an object and int representing the weight of chance to be returned
 type WeightedItem struct {
-	item   interface{}
-	weight int
+	Item   interface{}
+	Weight int
 }
